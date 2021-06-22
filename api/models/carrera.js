a667ff51c0183f1081bd740ee49fe0cfc427eea4
Ubuntu 18.04 <mmonescao@gmail.com>
2021-06-22 16:23:57 -0700

    1ra iteracion

--- api/models/carrera.js
+++ api/models/carrera.js
@@ -3,6 +3,8 @@ module.exports = (sequelize, DataTypes) => {
   const carrera = sequelize.define('carrera', {
     nombre: DataTypes.STRING
   }, {});
-  
+  carrera.associate = function(models) {
+    // associations can be defined here
+  };
   return carrera;
 };
